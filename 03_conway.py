f916dacc870bd80f081a997119a17bb5f8f39f29
blenderlab <thomas@blenderlab.fr>
2020-10-01 12:22:48 +0200

    adding curse

--- 03_conway.py
+++ 03_conway.py
@@ -9,27 +9,23 @@ OFF = 0
 # size of array :
 N=10
 
-# define a 2D array (10x10):
-grid = numpy.array([
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,1,0,0,0,1,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,1,0,0,0],
-    [0,0,0,0,0,1,0,1,0,0],
-    [0,0,0,0,0,0,0,1,1,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,1,0],
-    [0,0,0,0,0,0,0,0,0,0]
-    ])
+def randomGrid(N):
+    g = numpy.random.choice([ON,OFF], N*N, p=[0.2,0.8]).reshape(N,N)
+    return g
+
+# define a 2D array (NxN):
+grid = randomGrid(N)
 
 #Start pyGame Engine :
 pygame.init()
-screen = pygame.display.set_mode((300, 300))
+# SEt screen size to 300 x 300 px
+screen = pygame.display.set_mode((600, 600))
+#start pygame's clock :
 clock = pygame.time.Clock()
+#timer for user input :
 pygame.time.set_timer(pygame.USEREVENT, 1500)
 
-CELLSIZE = int(300/N)
+CELLSIZE = int(600/N)
 H_CELLSIZE = int(CELLSIZE / 2)
 
 
@@ -69,25 +65,25 @@ def update(g,N):
 
 
 def board_to_scr(x,y):
-    
+
     return (int(x * CELLSIZE + H_CELLSIZE),int( y * CELLSIZE + H_CELLSIZE))
 
 
 while True:
-	# Check if we want to exit : 
-	if pygame.event.get(pygame.QUIT): 
-		break
+    # Check if we want to exit :
+    if pygame.event.get(pygame.QUIT):
+        break
 
-	# Let's clean the screen :
-	screen.fill(pygame.color.Color('white'))
-	for numligne in range(0,N):
-		for numcol in range(0,N):
-			if grid[numcol,numligne]==ON:
-				pygame.draw.circle(screen, (255,0,255), board_to_scr(numcol,numligne),int(H_CELLSIZE), 0)
-	pygame.display.flip()
-	grid = update(grid,N)
-	clock.tick(60)
-	
+    # Let's clean the screen :
+    screen.fill(pygame.color.Color('white'))
+    for numligne in range(0,N):
+        for numcol in range(0,N):
+            if grid[numcol,numligne]==ON:
+                pygame.draw.circle(screen, (255,0,255), board_to_scr(numcol,numligne),int(H_CELLSIZE), 0)
+    pygame.display.flip()
+    grid = update(grid,N)
+    clock.tick(60)
+
 
 
 
